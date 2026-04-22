00cd69bf8e1ad6e4e53410afa11ed9a78086aeec
d-levchenko <dl.qubb@gmail.com>
2026-04-22 18:19:10 +0300

    style and structure updates;

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  todoInput: document.querySelector('.todo-input'),
+  todoCounter: document.querySelector('.todo-counter'),
+  clearCompletedBtn: document.querySelector('.todo-list-button'),
+  filtersList: document.querySelector('.filters-list'),
+  todoList: document.querySelector('.todo-list'),
+  creationWrapper: document.querySelector('.creation-wrapper'),
+};
